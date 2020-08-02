9fbd00d82ef81048b3fd839387fc40e54e199498
unknown <daljitsingh365@yahoo.com>
2020-08-02 02:37:24 -0700

    user module completed

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 	$(".passwordErrorField").hide();
 	$(".errorField").hide();
+	$(".forgotPasswordErrorField").hide();
+	$(".forgotPasswordMessageField").hide();
+	$(".loginErrorField").hide();
 	$(document).find("#submitUser").on("click",function(){
 
 		if($("#firstName").val() == "" || $("#lastName").val() == "" || $("#email").val() == "" || $("#phone").val()=="" || $("#pwd").val() == "" ){
@@ -36,6 +39,52 @@ $(document).ready(function(){
 			
 
 			
+		}
+		
+	});
+
+	$(document).find("#loginUser").on("click",function(){
+
+		if($(".login_email").val() == "" || $(".login_pwd").val() == "" ){
+			$(".loginErrorField").show();
+		}
+		else {
+			$(".loginErrorField").hide();
+			
+			var user = {email:"",password:""};
+			
+			user.email = $(".login_email").val();
+			user.password = $(".login_pwd").val();
+			$.ajax({
+		        url: 'http://localhost:3333/user/login',
+		        data: user,
+		        type: 'POST',
+		        dataType: "json",
+		        success: function (data) {
+		            if(typeof data.data !== "undefined"){
+		            	$(".signInButton").hide();
+		            	$(".signInLi").append('<a class="nav-link"> Hello '+data.data.first_name+'</a>');
+		            	$("#signin").modal('hide');
+		            } else {
+
+		            }
+		        },
+		        error: function (xhr, status, error) {
+		            console.log('Error: ' + error.message);
+		        },
+		    });
+		}
+		
+	});
+
+	$(document).find("#sendEmail").on("click",function(){
+		if($(".forgot_password_email").val() == "" ){
+			$(".forgotPasswordErrorField").show();
+		}
+		else {
+			$(".forgotPasswordErrorField").hide();
+			$(".forgotPasswordMessageField").show();
+			$(".forgot_password_email").val("");
 		}
 		
 	});
